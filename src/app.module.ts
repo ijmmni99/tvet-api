d5dbb70670ac6b580ec57b1e6640819284e7375b
ijmmni99 <nasharul99@gmail.com>
2021-10-26 22:41:07 +0800

    Update Teacher

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import config from 'ormconfig';
 import { Student } from './student/entities/student.entity';
 import { StudentService } from './student/student.service';
 import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
-    StudentModule
+    StudentModule,
+    TeacherModule
   ],
   controllers: [AppController],
   providers: [AppService],
